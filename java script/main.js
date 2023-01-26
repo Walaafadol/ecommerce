1ba79428952950755b5d9a30c1719880b8782eb2
walaafadol <walaafadul7@gmail.comgit push origin maingit push origin maingit push origin mastergit config --global user.email walaafadul7@gmail.com>
2023-01-27 00:17:17 +0200

    first commit

---
+++ java script/main.js
@@ -0,0 +1,9 @@
+let bar = document.getElementById("bar");
+let links = document.getElementById("links");
+let close = document.getElementById("close");
+function openMenu() {
+  links.style.right = "0";
+}
+function closeMenu() {
+  links.style.right = "-300px";
+}
